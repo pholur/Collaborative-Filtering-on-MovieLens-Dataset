00f6246aae70965a1b1194ca07a4aa90bff0ea32
Ravi Teja <raviteja1058@gmail.com>
2019-02-09 06:39:20 +0000

    q15 included

--- Project3_q10_14.py
+++ Project3_q10_14.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 from Project3_1234 import *
 import csv
 from surprise import KNNWithMeans
-from surprise.model_selection import KFold
+from surprise.model_selection import *
 from surprise.model_selection import cross_validate
 from surprise import Dataset
 from surprise import Reader
 from surprise import SVD
 from surprise import accuracy
-
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn import metrics
 
 file_path = './ml-latest-small/ratings.csv'
 reader = Reader(line_format='user item rating timestamp', sep=',', rating_scale=(0.5, 5), skip_lines=1)
@@ -120,6 +122,7 @@ for i in x_axis:
     rmse_unpopular_store.append(s)
 
 
+
 pd.DataFrame(rmse_unpopular_store).to_csv("rmse_unpopular_store_13.csv")
 plotgraphs(x_axis,rmse_unpopular_store,'K','Mean RMSE scores','Plot of unpopular movies')
 
@@ -150,3 +153,43 @@ for i in x_axis:
 
 pd.DataFrame(rmse_highvar_store).to_csv("rmse_highvar_store_13.csv")
 plotgraphs(x_axis,rmse_highvar_store,'K','Mean RMSE scores','Plot of high variance movies')
+
+
+
+######################### ROc plots ###################################
+
+def plot_roc(fpr, tpr,t):
+    fig, ax = plt.subplots()
+
+    roc_auc = auc(fpr, tpr)
+
+    ax.plot(fpr, tpr, lw=2, label='area under curve = %0.4f' % roc_auc)
+
+    ax.grid(color='0.7', linestyle='--', linewidth=1)
+
+    ax.set_xlim([-0.1, 1.1])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate', size=15)
+    ax.set_ylabel('True Positive Rate', size=15)
+    ax.legend(loc="lower right")
+
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontsize(15)
+    plt.title('Threshold  = %0.2f' % t, size=15)
+    plt.show()
+
+
+Threshold = [2.5, 3, 3.5, 4]
+best_k = 20
+trainset, testset = train_test_split(data, test_size=0.1, train_size=None, random_state=None, shuffle=True)
+algo = KNNWithMeans(k=best_k, sim_options=sim_options,verbose=True)
+algo.fit(trainset)
+predictions = algo.test(testset)
+trui = [ getattr(r,'r_ui') for r in predictions ]
+est = [ getattr(r,'est') for r in predictions ]
+
+for t in Threshold:
+    trui_bin = [1 if r > t else 0 for r in trui]
+    # est_bin = [1 if r > t else 0 for r in est]
+    fpr, tpr, _ = metrics.roc_curve(trui_bin,est)
+    plot_roc(fpr,tpr,t)
